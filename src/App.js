269ad8ffec3888bd7bcdc6fb11e58dc44c4d19c4
hfarazakram <hfarazakram1991@gmail.com>
2023-09-17 11:20:20 +0500

    new update

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import Layout from "./Layout";
+
+
+function App() {
+  return (
+    <>
+    <Layout>
+      <p>This is page content</p>
+    </Layout>
+    </>
+  );
+}
+
+export default App;
---
+++ src/Layout/footer.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Footer = () => {
+  return (
+   <>
+   <h1>
+       This is footer
+   </h1>
+   </>
+  )
+}
+
+export default Footer
